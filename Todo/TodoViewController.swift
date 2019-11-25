a99710cbabd535dc39c510b8580c952b1888af0c
Muhammad Usman <usmanirfan996@gmail.com>
2019-11-25 18:23:57 +0500

    list completed with datasource and delegate mehods

---
+++ Todo/TodoViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Muhammad Usman on 25/11/2019.
+//  Copyright © 2019 Muhammad Usman. All rights reserved.
+//
+
+import UIKit
+
+class TodoViewController: UITableViewController {
+    
+    var itemsArray = ["Transworld Bill", "Buy Eggs", "Phone Repair"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        
+        cell.textLabel?.text = itemsArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
